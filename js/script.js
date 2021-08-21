7ff23612b283f1d0a076732796241a9107705db6
Triumph <Triumphjo123@gmail.com>
2021-08-21 05:09:10 +0100

    updated the resposiveness and fixed the carousel

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
-const img = document.querySelectorAll('.header__image-slider figure img');
-const imgActive = document.querySelector(".header__image-slider figure img.active");
 const nextBtn = document.querySelector('#Next');
 const previousBtn = document.querySelector("#Previous");
+const carousel = document.querySelectorAll('#carousel');
 
-let activeImageIndex = 0;
+let styleMarginRight = 0;
 
 function nextImageHandler(e) {
     e.preventDefault();
-    activeImageIndex = ++activeImageIndex;
-    if (activeImageIndex >= 3) {
-        activeImageIndex = 0;
-        img[activeImageIndex + 2].classList.remove("active");
-        img[activeImageIndex].classList.add("active");
-    } else {
-        img[activeImageIndex - 1].classList.remove("active");
-        img[activeImageIndex].classList.add('active');
+
+    if(styleMarginRight >= 0){
+        styleMarginRight += 100;
+        
+        carousel[0].style.right = `${styleMarginRight}%`;
+    }
+    if(styleMarginRight > 200){
+        styleMarginRight = 0;
+        
+        carousel[0].style.right = `${styleMarginRight}%`;
     }
 }
 
 function previousImageHandler(e) {
     e.preventDefault();
-    activeImageIndex = --activeImageIndex;
-    if (activeImageIndex < 0) {
-        activeImageIndex = 2;
-        img[activeImageIndex - 2].classList.remove("active");
-        img[activeImageIndex].classList.add("active");
-    } else {
-        img[activeImageIndex + 1].classList.remove("active");
-        img[activeImageIndex].classList.add("active");
+    if (styleMarginRight >= 0) {
+      styleMarginRight -= 100;
+      carousel[0].style.right = `${styleMarginRight}%`;
+    }
+    if (styleMarginRight < 0) {
+      styleMarginRight += 300;
+      carousel[0].style.right = `${styleMarginRight}%`;
     }
 }
 
